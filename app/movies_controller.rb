c8791a7942f53c16d27dde110d8a63d50287c4ae
Sachin Mahajan <sachinm78@gmail.com>
2019-04-15 22:49:57 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,7 +31,6 @@ def can_be_created_in_a_block(args = {:title => "Home Alone", :release_date => 1
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
-    m.save
   end
 end
 
